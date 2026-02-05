97a28cadfbd31221fec35b2ebc8ad0efacacc5bc
ninngiorgadze <ninngiorgadze@gmail.com>
2026-02-05 12:17:33 +0400

    Auto-generated commit

---
+++ chunk-R3YF3CQP.js
@@ -0,0 +1,1 @@
+import{J as e,f as t,h as i}from"./chunk-HRCNI5R5.js";var r=class o{static \u0275fac=function(m){return new(m||o)};static \u0275mod=i({type:o});static \u0275inj=t({imports:[e]})};export{r as a};
