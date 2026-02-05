723149422aff4dcfabc93dd495bd04d1af6d27c7
ninngiorgadze <ninngiorgadze@gmail.com>
2026-02-05 13:28:33 +0400

    Fix routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy } // <--- add this
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
