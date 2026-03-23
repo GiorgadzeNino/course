42610d9d776b1b3dc8450c4c339c3a0666fd9830
ninngiorgadze <ninngiorgadze@gmail.com>
2026-03-23 14:38:31 +0400

    Add services part 1

--- src/app/features/angular/page/lecture-4/lecture-4.component.ts
+++ src/app/features/angular/page/lecture-4/lecture-4.component.ts
@@ -2,14 +2,75 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lecture-4',
-  template: `
-    <article class="lecture">
-      <h1>Module 4: RxJS & State Management</h1>
-      <p>This module will cover Observables, Subjects, Operators, and State Management.</p>
-      <p>Content coming soon...</p>
-    </article>
-  `,
-  styles: []
+  templateUrl: './lecture-4.component.html',
+  styleUrl: './lecture-4.component.scss'
 })
-export class Lecture4Component {}
+export class Lecture4Component {
+  httpUserServiceCode = `import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+}
+`;
+
+  httpUsersComponentCode = `import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+
+@Component({
+  selector: 'app-users',
+  template: \`
+    <ul>
+      <li *ngFor="let user of users">{{ user.name }}</li>
+    </ul>
+  \`
+})
+export class UsersComponent implements OnInit {
+  users: any[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
+    });
+  }
+}
+`;
+
+  componentExampleCode = `import { Component } from '@angular/core';
+import { MyServiceService } from './my-service.service';
+
+@Component({
+  selector: 'app-home',
+  template: \`<h1>{{ message }}</h1>\`
+})
+export class HomeComponent {
+  message: string = '';
+
+  constructor(private myService: MyServiceService) {}
+
+  ngOnInit() {
+    this.message = this.myService.greet('Angular');
+  }
+}
+`;
+
+  demoUsers = [
+    { name: 'Leanne Graham' },
+    { name: 'Ervin Howell' },
+    { name: 'Clementine Bauch' }
+  ];
+}
 
